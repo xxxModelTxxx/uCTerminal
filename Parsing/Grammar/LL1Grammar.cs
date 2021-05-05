56f03e86049d389a4b00ff62b0788c2faf52d02d
Marcin Pietrzak <marcin-pietrzak@outlook.com>
2021-05-05 22:13:32 +0200

    Update 3

--- Parsing/Grammar/LL1Grammar.cs
+++ Parsing/Grammar/LL1Grammar.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace EMP.Syntax.Grammar
@@ -23,50 +22,168 @@ namespace EMP.Syntax.Grammar
                 return _parseTable[terminal.GetHashCode()][nonTerminal.GetHashCode()];
             }
         }
-        private IEnumerable<Symbol> Firsts(Symbol symbol)
+        private bool IsNullable(Symbol symbol)
         {
-            var output = new Dictionary<int, Symbol>();
-
-            if (symbol.IsTerminal)
+            return Nullable()[symbol];
+        }
+        private IDictionary<Symbol, ICollection<Symbol>> First()
+        {
+            // Initialize
+            var output = new Dictionary<Symbol, HashSet<Symbol>>();
+            var nullables = Nullable();
+            bool changed = true;
+            foreach (Symbol s in Symbols)
             {
-                output.Add(symbol.GetHashCode(), symbol);
+                output.Add(s, new HashSet<Symbol>());
             }
-            else
+
+            // If X is terminal, then FIRST(X) is {X}
+            foreach (Symbol s in Symbols)
             {
+                if (s.IsTerminal)
+                {
+                    output[s].Add(s);
+                }
+            }
+
+            // If X ::= e is a production, then add e to FIRST(X)
+            foreach (Rule r in Rules)
+            {
+                if (r.Right.First().GetHashCode() == Symbol.Epsilon.GetHashCode())
+                {
+                    output[r.Left.First()].Add(Symbol.Epsilon);
+                }
+            }
+
+            // If X is nonterminal and X :== Y1 Y2 ... Yk. is a production, then place a in FIRST(X) if for some i, a is in
+            // FIRST(Yi), and e is in all of FIRST(Y1), ... , FIRST(Yi - 1); that is, Y1, ... ,Yi - 1 Þ e. If e is in FIRST(Yj) for
+            // all j = 1, 2, ... , k, then add e to FIRST(X).
+            while (changed)
+            {
+                changed = false;
+
                 foreach (Rule r in Rules)
                 {
-                    if (symbol.GetHashCode() == r.Left.First().GetHashCode())
+                    foreach (Symbol s1 in r.Right)
                     {
-                        Symbol s1 = r.Right.First();
                         if (s1.IsTerminal)
                         {
-                            output.TryAdd(s1.GetHashCode(), s1);
+                            changed = output[r.Left.First()].Add(s1);
+                            break;
+                        }
+                        else if (nullables[s1])
+                        {
+                            foreach (Symbol s2 in output[s1])
+                            {
+                                if (s2.GetHashCode() != Symbol.Epsilon.GetHashCode())
+                                {
+                                    changed |= output[r.Left.First()].Add(s2);
+                                }
+                            }
                         }
                         else
                         {
-                            foreach (Symbol s2 in Firsts(s1))
+                            foreach (Symbol s2 in output[s1])
                             {
-                                output.TryAdd(s2.GetHashCode(), s2);
+                                changed |= output[r.Left.First()].Add(s2);
                             }
+                            break;
                         }
                     }
                 }
             }
-            return output.Values;
+
+            return (IDictionary <Symbol, ICollection<Symbol>>)output;
         }
-        private IEnumerable<Symbol> Follows(Symbol symbol)
+        private IDictionary<Symbol, ICollection<Symbol>> Follow()
         {
-            throw new NotImplementedException();
+            // Initialize
+            var output = new Dictionary<Symbol, HashSet<Symbol>>();
+            var firsts = First();
+            bool changed = true;
+            foreach (Symbol s in NonTerminals)
+            {
+                output.Add(s, new HashSet<Symbol>());
+            }
+
+            // Place $ in FOLLOW(S), where S is the start symbol and $ is the input right endmarker.
+            output[Symbol.Start].Add(Symbol.End);
+
+            // If there is a production A ::= aBb, then everything in FIRST(b), except for e, is placed in FOLLOW(B).
+            foreach (Rule r in Rules)
+            {
+                Symbol sp = null;
+                foreach (Symbol sc in r.Right)
+                {
+                    if (sp != null)
+                    {
+                        foreach (Symbol s in firsts[sc])
+                        {
+                            if (s != Symbol.Epsilon)
+                            {
+                                output[sp].Add(s);
+                            }
+                        }
+                    }
+                    if (!sc.IsTerminal)
+                    {
+                        sp = sc;
+                    }
+                    else
+                    {
+                        sp = null;
+                    }
+                }
+            }
+
+            // If there is a production A ::= aB, or a production A ::= aBb where FIRST(b) contains e (i.e., b ::= e),
+            // then everything in FOLLOW(A) is in FOLLOW(B).
+            while(changed)
+            {
+                changed = false;
+                Symbol sp = null;
+                foreach (Rule r in Rules)
+                {
+                    foreach (Symbol sc in r.Right)
+                    {
+                        if (sp != null && firsts[sc].Contains(Symbol.Epsilon))
+                        {
+                            foreach(Symbol s in output[r.Left.First()])
+                            {
+                                changed |= output[sp].Add(s);
+                            }
+                        }
+                        if (!sc.IsTerminal)
+                        {
+                            sp = sc;
+                        }
+                        else
+                        {
+                            sp = null;
+                        }
+                    }
+                    if (!r.Right.Last().IsTerminal)
+                    {
+                        foreach (Symbol s in output[r.Left.First()])
+                        {
+                            changed |= output[sp].Add(s);
+                        }
+                    }
+                }
+            }
+
+            return (IDictionary<Symbol, ICollection<Symbol>>)output;
+
         }
-        private bool IsNullabe(Symbol symbol)
+        private IDictionary<Symbol, bool> Nullable()
         {
             var changed = true;
-            var nullables = new Dictionary<int, bool>();
+            var output = new Dictionary<Symbol, bool>();
             
             // Create collection of nullable
             foreach(Symbol s in Symbols)
             {
-                nullables.Add(s.GetHashCode(), false);
+                output.Add(s, false);
             }
 
             // Check for rules containig only epsilon on RHS
@@ -74,7 +191,7 @@ namespace EMP.Syntax.Grammar
             {
                 if (r.Right.First().GetHashCode() == Symbol.Epsilon.GetHashCode())
                 {
-                    nullables[r.Left.First().GetHashCode()] = true;
+                    output[r.Left.First()] = true;
                 }
             }
 
@@ -84,24 +201,24 @@ namespace EMP.Syntax.Grammar
                 changed = false;
                 foreach(Rule r in Rules)
                 {
-                    if (!nullables[r.Left.First().GetHashCode()])
+                    if (!output[r.Left.First()])
                     {
                         bool state = true;
                         foreach (Symbol s in r.Right)
                         {
-                            state &= nullables[s.GetHashCode()];
+                            state &= output[s];
                         }
                         if (state)
                         {
                             changed = true;
-                            nullables[r.Left.First().GetHashCode()] = state;
+                            output[r.Left.First()] = state;
                         }
                     }
                 }
             }
 
             // Return result
-            return nullables[symbol.GetHashCode()];
+            return output;
         }
     }
 }
